fc7d5a11a2ea1166f0fb0a15bfba9c3addb0792e
khensolomon <khensolomon@gmail.com>
2017-12-06 14:25:14 +0100

    LAMP

--- config/routeController.php
+++ config/routeController.php
@@ -1,44 +1,46 @@
 <?php
-namespace app;
-class routeController extends \letId\request\http
+namespace app
 {
-  /**
-  * Application (folder) => hostname (regex without slashs)
-  * @var array/string
-  */
-  protected $application = array(
-    'test'=>'example.',
-    // 'test'=>'example.localhost',
-    // 'test'=>array(
-    //     'example.com','.example.com','.example.'
-    // ),
-    // 'test'=>'test.',
-    'test'=>''
-  );
-  /**
-  * Common directory rewrite!
-  * @var array
-  */
-  // protected $rewrite = array();
-  /**
-  * Setting!
-  * @var array
-  */
-  // protected $setting = array();
-  /**
-  * Common directory
-  * @var array
-  */
-  // protected $directory = array(
-  //   'template'=>'template',
-  //   'language'=>'language'
-  // );
-  /**
-  * Common configurations
-  * @var array
-  * NOTE: application's namespace, ANS can not be modified!
-  */
-  protected $configuration = array(
-    'ANS'=>__NAMESPACE__
-  );
+  class routeController extends \letId\request\http
+  {
+    /**
+    * Application (folder) => hostname (regex without slashs)
+    * @var array/string
+    */
+    protected $application = array(
+      'test'=>'example.',
+      // 'test'=>'example.localhost',
+      // 'test'=>array(
+      //     'example.com','.example.com','.example.'
+      // ),
+      // 'test'=>'test.',
+      'test'=>''
+    );
+    /**
+    * Common directory rewrite!
+    * @var array
+    */
+    // protected $rewrite = array();
+    /**
+    * Setting!
+    * @var array
+    */
+    // protected $setting = array();
+    /**
+    * Common directory
+    * @var array
+    */
+    // protected $directory = array(
+    //   'template'=>'template',
+    //   'language'=>'language'
+    // );
+    /**
+    * Common configurations
+    * @var array
+    * NOTE: application's namespace, ANS can not be modified!
+    */
+    protected $configuration = array(
+      'ANS'=>__NAMESPACE__
+    );
+  }  
 }
